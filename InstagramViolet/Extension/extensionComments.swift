4a2e3d1f78072db2b953db0a98067b2314bd6e4d
Владислав Артюхов <severuswolf@icloud.com>
2024-12-27 02:54:04 +0300

    [INST - 16] - new NotificationCenter

--- InstagramViolet/Extension/extensionComments.swift
+++ InstagramViolet/Extension/extensionComments.swift
@@ -18,15 +18,34 @@ extension CommentsController {
                          selector: #selector(keyboardWillHide),
                          name: UIResponder.keyboardWillHideNotification,
                          object: nil)
+        NotificationCenter.default
+            .addObserver(self,
+                         selector: #selector(keyboardDidShow),
+                         name: UIResponder.keyboardDidShowNotification,
+                         object: nil)
     }
 
     @objc private func keyboardWillShow(notification: Notification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             containerView.frame = CGRect(x: 0, y: view.frame.height - keyboardSize.height - 50 , width: view.frame.width, height: 80)
+            
         }
     }
 
     @objc private func keyboardWillHide(notification: Notification) {
         containerView.frame = CGRect(x: 0, y: view.frame.height - 80, width: view.frame.width, height: 80)
+    }
+    
+    @objc private func keyboardDidShow(notification: Notification) {
+        if comments.count > 1 {
+            let lastRowIndex = IndexPath(row: comments.count - 1, section: 0)
+            let cellAttributes = collectionView.layoutAttributesForItem(at: lastRowIndex)
+            if let cellFrame = cellAttributes?.frame {
+                // Преобразуем координаты в координаты содержимого
+                var visibleRect = collectionView.convert(cellFrame, from: collectionView)
+                visibleRect.size.height += 60
+                collectionView.scrollRectToVisible(visibleRect, animated: true)
+            }
+        }
     }
 }
