649f04fdcd4cf6c4fa91591a225fc55e488d1fb1
Владислав Артюхов <severuswolf@icloud.com>
2025-01-29 10:30:45 +0300

    [INST - 28] - bugfix

--- InstagramViolet/LikedPostsMVP/ServiceLayer/FirebaseService.swift
+++ InstagramViolet/LikedPostsMVP/ServiceLayer/FirebaseService.swift
@@ -6,12 +6,32 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
 protocol FirebaseServiceProtocol {
-    func fetchPosts(completion: @escaping (Result<[LikedPost]?, Error>) -> Void)
+    func fetchUser(completion: @escaping (Result<User?, Error>) -> Void)
+    func fetchPostsWithUser(completion: @escaping (Result<[LikedPost]?, Error>) -> Void)
 }
 
 class FirebaseService: FirebaseServiceProtocol {
-    func fetchPosts(completion: @escaping (Result<[LikedPost]?, any Error>) -> Void) {
+    
+    func fetchUser(completion: @escaping (Result<User?, Error>) -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value) { snapshot in
+            guard let dictionary = snapshot.value as? [String: Any] else { return }
+            let user = User(uid: uid, dictionary: dictionary)
+            completion(.success(user))
+        } withCancel: { error in
+            completion(.failure(error))
+            print(" Failed to fetch User: \(error)")
+        }
+    }
+    
+    func fetchPostsWithUser(completion: @escaping (Result<[LikedPost]?, any Error>) -> Void) {
+        fetchUser { user in
+            Database.database().reference().child(<#T##pathString: String##String#>)
+        }
         
     }
     
